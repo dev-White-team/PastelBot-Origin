deb40c8d779d477d556b246638ff45d696b055b3
Starcea <stardev.uwu@gmail.com>
2023-01-29 23:29:16 +0900

    docs: fix

--- functions/simple.py
+++ functions/simple.py
@@ -38,7 +38,7 @@ class SimpleGame(Cog):
     async def dice(
             self,
             ctx: ApplicationContext,
-            firstn: Option(int, "첫번째 숫자를 정하세요. 두번째 숫자가 없을 경우 범위는 1 ~ firstn으로 결정됩니다.", name="범위1"),
+            firstn: Option(int, "첫번째 숫자를 정하세요. 두번째 숫자가 없을 경우 범위는 1 ~ 범위1 으로 결정됩니다.", name="범위1"),
             secondn: Option(
                 int, "두번째 숫자가 있을 경우 범위는 firstn ~ secondn으로 결정됩니다. ", required=False, name="범위2"
             ),

7ed35a176a9f9d15b788681c2e1aa31470158490
Starcea <stardev.uwu@gmail.com>
2023-01-31 14:55:14 +0900

    feat: show version in status message

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from typing import Final
+from constants import VERSION
 
-STATUS: Final[str] = "BETA"  # 상태 메시지
+STATUS: Final[str] = f"BETA RUNNING: {VERSION}"  # 상태 메시지
 COLOR: Final[int] = 0xe1c7ef  # 일반 임베드 색상
 BAD: Final[int] = 0xed4245  # 오류/위험 임베드 색상
